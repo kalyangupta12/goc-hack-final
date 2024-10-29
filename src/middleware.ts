42fee7b4478a79096620e76214cf25b33f01eb39
Kalyan Gupta <90916009+kalyangupta12@users.noreply.github.com>
2024-10-29 23:33:12 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.)", "/sign-up(.)"])
-
-export default clerkMiddleware(async (auth, request) => {
-  try {
-    if (!isPublicRoute(request)) {
-      await auth.protect()
-    }
-  } catch (error) {
-    console.error("Middleware error:", error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 }
