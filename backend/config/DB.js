46547b4afda5e5c18863d66e868a56cca12d3539
Kalyan Gupta <90916009+kalyangupta12@users.noreply.github.com>
2024-10-30 00:08:25 +0530

    Update DB.js

--- backend/config/DB.js
+++ backend/config/DB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connDB = async () => {
   try {
-    const conn = await mongoose.connect(PROCESS.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
